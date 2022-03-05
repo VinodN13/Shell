76e639171eb39cc92ea2943eabf26da8de26e94f
Vinod <ubuntu@ip-172-31-46-154.us-east-2.compute.internal>
2022-03-05 07:06:48 +0000

    Shell script file

---
+++ date.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Program to print current date, time, user name and current working directory
+
+echo "Current date is: "
+date
+echo "Username is: "
+whoami
+echo "Current working directory is: "
+pwd
+
